51c97cb31565afaca2c38e3a412b97efe6fef186
Alex <alex@wendte.tech>
2020-12-21 16:53:02 -0600

    adds robotjs and uses it

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
@@ -18,6 +19,8 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  require('robotjs').typeString('23+37=');
 };
 
 // This method will be called when Electron has finished
